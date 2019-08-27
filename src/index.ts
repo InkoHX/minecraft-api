4ee49adc08a18f0c4646cc1447360dd8e429eb21
InkoHX <inkohx@gmail.com>
2019-08-27 22:13:28 +0900

    setup eslint

--- src/index.ts
+++ src/index.ts
@@ -1,41 +1,41 @@
-import fetch from "node-fetch";
+import fetch from 'node-fetch'
 
-export type MojangServiceStatus = "green" | "yellow" | "red"
+export type MojangServiceStatus = 'green' | 'yellow' | 'red'
 
-export type MojangService = "minecraft.net"
-  | "session.minecraft.net"
-  | "account.mojang.com"
-  | "authserver.mojang.com"
-  | "sessionserver.mojang.com"
-  | "api.mojang.com"
-  | "textures.minecraft.net"
-  | "mojang.com"
+export type MojangService = 'minecraft.net'
+  | 'session.minecraft.net'
+  | 'account.mojang.com'
+  | 'authserver.mojang.com'
+  | 'sessionserver.mojang.com'
+  | 'api.mojang.com'
+  | 'textures.minecraft.net'
+  | 'mojang.com'
 
 export interface UserProfileResponse {
-  id: string
-  name: string
-  demo?: boolean
-  legacy?: boolean
+  id: string;
+  name: string;
+  demo?: boolean;
+  legacy?: boolean;
 }
 
 /**
  * The changedToAt field is a Java timestamp in milliseconds.
  */
 export interface NameHistoryResponse {
-  name: string
-  changedToAt?: number
+  name: string;
+  changedToAt?: number;
 }
 
 export interface SkinDataResponse {
-  id: string
-  name: string
+  id: string;
+  name: string;
   properties: [
     {
-      name: string
-      value: string
-      signature?: string
+      name: string;
+      value: string;
+      signature?: string;
     }
-  ]
+  ];
 }
 
 export class MinecraftAPIError extends Error { }
@@ -45,17 +45,17 @@ export default class MinecraftAPI {
 
   public uuid?: string
   public currentName?: string
-  public legacy: boolean = false
-  public demo: boolean = false
+  public legacy = false
+  public demo = false
 
   public history?: NameHistoryResponse[]
   public skinData?: SkinDataResponse;
 
-  public constructor(name: string) {
+  public constructor (name: string) {
     this.name = name
   }
 
-  public async fetchProfile(): Promise<UserProfileResponse> {
+  public async fetchProfile (): Promise<UserProfileResponse> {
     const data = await fetch(`https://api.mojang.com/users/profiles/minecraft/${this.name}`).catch(() => null)
     if (!data) throw new MinecraftAPIError('Fetch error.')
     if (data.status !== 200) throw new MinecraftAPIError(`Status Code: ${data.status} | ${data.statusText}`)
@@ -69,7 +69,7 @@ export default class MinecraftAPI {
     return profile
   }
 
-  public async fetchNamehistory(): Promise<NameHistoryResponse[]> {
+  public async fetchNamehistory (): Promise<NameHistoryResponse[]> {
     if (!this.uuid) throw new MinecraftAPIError()
     const data = await fetch(`https://api.mojang.com/user/profiles/${this.uuid}/names`).catch(() => null)
     if (!data) throw new MinecraftAPIError('Fetch error.')
@@ -82,7 +82,7 @@ export default class MinecraftAPI {
     return nameHistory
   }
 
-  public async fetchSkinData(unsigned: boolean = true): Promise<SkinDataResponse> {
+  public async fetchSkinData (unsigned = true): Promise<SkinDataResponse> {
     if (!this.uuid) throw new MinecraftAPIError()
     const data = await fetch(`https://sessionserver.mojang.com/session/minecraft/profile/${encodeURIComponent(this.uuid)}?unsigned=${unsigned}`).catch(() => null)
     if (!data) throw new MinecraftAPIError('Fetch error.')
@@ -95,7 +95,7 @@ export default class MinecraftAPI {
     return profile
   }
 
-  public async fetch(): Promise<void> {
+  public async fetch (): Promise<void> {
     await this.fetchProfile()
     await this.fetchProfile()
     await this.fetchSkinData()
@@ -104,7 +104,7 @@ export default class MinecraftAPI {
   /**
    * Returns status of various Mojang services.
    */
-  public static async getServiceStatus(): Promise<{[key in MojangService]: MojangServiceStatus}[]> {
+  public static async getServiceStatus (): Promise<{[key in MojangService]: MojangServiceStatus}[]> {
     const data: {[key in MojangService]: MojangServiceStatus}[] | null = await fetch('https://status.mojang.com/check')
       .then((res) => res.json())
       .catch(() => null)
@@ -116,7 +116,7 @@ export default class MinecraftAPI {
   /**
    * Returns a list of SHA1 hashes used to check server addresses against when the client tries to connect.
    */
-  public static async getBlockedServers(): Promise<string> {
+  public static async getBlockedServers (): Promise<string> {
     const data = await fetch('https://sessionserver.mojang.com/blockedservers').catch(() => null)
     if (data) return data.text()
     else throw new MinecraftAPIError('Fetch error.')
